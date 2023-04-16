13d9ad5895c4f9a467642e88607aa7cd2c09072e
claudio-py <101283035+claudio-py@users.noreply.github.com>
2023-04-15 21:45:14 -0300

    update proj

--- main.js
+++ main.js
@@ -8,18 +8,82 @@ for (const element of toggle) {
 
 const links = document.querySelectorAll('nav ul li a')
 for (const link of links) {
-	link.addEventListener('click', function () {
+	link.addEventListener('click', () => {
 		nav.classList.remove('show')
 	})
 }
 
 // ============HEADER SHADOW=====
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-window.addEventListener('scroll', function () {
+
+function changeHeaderWhenScroll(){
+	const header = document.querySelector('#header')
+	const navHeight = header.offsetHeight
 	if (window.scrollY >= navHeight) {
 		header.classList.add('scroll')
 	} else {
 		header.classList.remove('scroll')
 	}
+}
+
+
+//========== TESTIMONIALS CARROSSEL===============
+
+const swiper = new Swiper('.swiper-container', {
+	slidesPerView: 1,
+	pagination: {
+		el: '.swiper-pagination'
+	},
+	mousewheel: true,
+	keyboard: true,
+	breakpoints:{
+		767:{
+			slidesPerView:2,
+			setWrapperSize:true,
+		}
+	}
 })
+
+// ===========SCROLL REVEAL==========
+const scrollReveal = ScrollReveal({
+	origin: 'top',
+	distance: '30px',
+	duration: 700,
+	reset: true
+})
+
+scrollReveal.reveal(
+	`
+#home .image, #home .text, 
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact.text, #contact .links,
+footer .brand, footer .social 
+`,
+	{ interval: 100 }
+)
+//  ============BOTÃO VOLTAR AO TOPO===========
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop(){
+if (window.scrollY >= 2000) {
+	backToTopButton.classList.add('show')
+} else {
+	backToTopButton.classList.remove('show')
+}
+}
+// ========WHEN SCROLL========
+window.addEventListener('scroll',()=>{
+	changeHeaderWhenScroll()
+	backToTop()
+})
+
+
+
+
+
+
+
+
+
+
+
