60e194c5e62dc91068c0e44e01a39d3e30c29376
claudio-py <101283035+claudio-py@users.noreply.github.com>
2023-04-16 00:02:24 -0300

    project finished

--- main.js
+++ main.js
@@ -14,10 +14,9 @@ for (const link of links) {
 }
 
 // ============HEADER SHADOW=====
-
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 function changeHeaderWhenScroll(){
-	const header = document.querySelector('#header')
-	const navHeight = header.offsetHeight
 	if (window.scrollY >= navHeight) {
 		header.classList.add('scroll')
 	} else {
@@ -71,10 +70,34 @@ if (window.scrollY >= 2000) {
 	backToTopButton.classList.remove('show')
 }
 }
+
+// MENU SECTION ACTIVATE
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection(){
+	const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+for (const section of sections) {
+const sectionTop = section.offsetTop
+const sectionHeight = section.offsetHeight
+const sectionId = section.getAttribute('id')
+const checkpointStart = checkpoint >= sectionTop
+const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+if (checkpointStart && checkpointEnd){
+document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('active')
+} else {
+document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('active')
+}
+}
+
+}
+
+
+
 // ========WHEN SCROLL========
 window.addEventListener('scroll',()=>{
 	changeHeaderWhenScroll()
 	backToTop()
+	activateMenuAtCurrentSection()
 })
 
 
