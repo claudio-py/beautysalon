b85f0f46aeff906702e63f8c5dada9ae1b049b76
Claudio Daniel <claudio.3777@outlook.com>
2024-06-28 11:38:49 -0300

    fixed layout width

--- main.js
+++ main.js
@@ -1,54 +1,53 @@
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle ')
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle ');
 for (const element of toggle) {
 	element.addEventListener('click', () => {
-		nav.classList.toggle('show')
-	})
+		nav.classList.toggle('show');
+	});
 }
 
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll('nav ul li a');
 for (const link of links) {
 	link.addEventListener('click', () => {
-		nav.classList.remove('show')
-	})
+		nav.classList.remove('show');
+	});
 }
 
 // ============HEADER SHADOW=====
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-function changeHeaderWhenScroll(){
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+function changeHeaderWhenScroll() {
 	if (window.scrollY >= navHeight) {
-		header.classList.add('scroll')
+		header.classList.add('scroll');
 	} else {
-		header.classList.remove('scroll')
+		header.classList.remove('scroll');
 	}
 }
-
 
 //========== TESTIMONIALS CARROSSEL===============
 
 const swiper = new Swiper('.swiper-container', {
 	slidesPerView: 1,
 	pagination: {
-		el: '.swiper-pagination'
+		el: '.swiper-pagination',
 	},
 	mousewheel: true,
 	keyboard: true,
-	breakpoints:{
-		767:{
-			slidesPerView:2,
-			setWrapperSize:true,
-		}
-	}
-})
+	breakpoints: {
+		767: {
+			slidesPerView: 2,
+			setWrapperSize: true,
+		},
+	},
+});
 
 // ===========SCROLL REVEAL==========
 const scrollReveal = ScrollReveal({
 	origin: 'top',
 	distance: '30px',
 	duration: 700,
-	reset: true
-})
+	reset: true,
+});
 
 scrollReveal.reveal(
 	`
@@ -60,53 +59,43 @@ scrollReveal.reveal(
 footer .brand, footer .social 
 `,
 	{ interval: 100 }
-)
+);
 //  ============BOTÃO VOLTAR AO TOPO===========
-const backToTopButton = document.querySelector('.back-to-top')
-function backToTop(){
-if (window.scrollY >= 2000) {
-	backToTopButton.classList.add('show')
-} else {
-	backToTopButton.classList.remove('show')
-}
+const backToTopButton = document.querySelector('.back-to-top');
+function backToTop() {
+	if (window.scrollY >= 2000) {
+		backToTopButton.classList.add('show');
+	} else {
+		backToTopButton.classList.remove('show');
+	}
 }
 
 // MENU SECTION ACTIVATE
-const sections = document.querySelectorAll('main section[id]')
-function activateMenuAtCurrentSection(){
-	const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+const sections = document.querySelectorAll('main section[id]');
+function activateMenuAtCurrentSection() {
+	const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
 
-for (const section of sections) {
-const sectionTop = section.offsetTop
-const sectionHeight = section.offsetHeight
-const sectionId = section.getAttribute('id')
-const checkpointStart = checkpoint >= sectionTop
-const checkpointEnd = checkpoint <= sectionTop + sectionHeight
-if (checkpointStart && checkpointEnd){
-document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('active')
-} else {
-document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('active')
+	for (const section of sections) {
+		const sectionTop = section.offsetTop;
+		const sectionHeight = section.offsetHeight;
+		const sectionId = section.getAttribute('id');
+		const checkpointStart = checkpoint >= sectionTop;
+		const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+		if (checkpointStart && checkpointEnd) {
+			document
+				.querySelector('nav ul li a[href*=' + sectionId + ']')
+				.classList.add('active');
+		} else {
+			document
+				.querySelector('nav ul li a[href*=' + sectionId + ']')
+				.classList.remove('active');
+		}
+	}
 }
-}
-
-}
-
-
 
 // ========WHEN SCROLL========
-window.addEventListener('scroll',()=>{
-	changeHeaderWhenScroll()
-	backToTop()
-	activateMenuAtCurrentSection()
-})
-
-
-
-
-
-
-
-
-
-
-
+window.addEventListener('scroll', () => {
+	changeHeaderWhenScroll();
+	backToTop();
+	activateMenuAtCurrentSection();
+});
